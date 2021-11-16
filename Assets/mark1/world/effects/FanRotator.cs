91e482f3e9f5356ab40e79adf125837ef908b4d4
Arne von Appen <arne.appen@limetec-biotechnologies.de>
2021-11-16 14:26:01 +0100

    position multiplayer fix?

---
+++ Assets/mark1/world/effects/FanRotator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace mark1.world.effects
+{
+    public class FanRotator : MonoBehaviour
+    {
+        public float speed = 15;
+        
+        private void Update()
+        {
+            transform.Rotate(0, speed * Time.deltaTime, 0);
+        }
+    }
+}
